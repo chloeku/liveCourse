7a29ef80513f1b305222af9e7045d17a12fbf4f9
chloeku <53157367+chloeku@users.noreply.github.com>
2020-12-30 20:41:08 +0800

    第六週 - 香水電商_V1

---
+++ week06/js/all.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $("#header").load("page/header.html");
+    $("#header-mold").load("page/header-mold.html");
+    $("#header-member").load("page/header-member.html");
+    $("#footer").load("page/footer.html");
+  });
+
+AOS.init({
+    useClassNames: true,
+    initClassName: false,
+    animatedClassName: 'animate__animated'
+    // animatedClassName: 'animated',
+});
